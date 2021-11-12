6e577309bbb649f5e1e095b8b2d368cb09246b25
mazeau <mazeau.e@husky.neu.edu>
2021-11-12 14:21:59 -0500

    working on fixing this script

--- plot-small-grid.py
+++ plot-small-grid.py
@@ -35,7 +35,7 @@ with sns.axes_style("whitegrid"):
     plt.xlim(carbon_range)
     plt.ylim(oxygen_range)
     plt.yticks(np.arange(-5.25,-3,0.5))
-plt.show()
+# plt.show()
 
 # just to double-check
 experiments = mesh.reshape((2,-1)).T
@@ -57,26 +57,42 @@ carbon_range2 = (carbon_range[0]-c_step/2, carbon_range[1]+c_step/2)
 oxygen_range2 = (oxygen_range[0]-c_step/2, oxygen_range[1]+c_step/2)
 extent2 = carbon_range2 + oxygen_range2
 
+# For close packed surfaces from
+# Abild-Pedersen, F.; Greeley, J.; Studt, F.; Rossmeisl, J.; Munter, T. R.;
+# Moses, P. G.; Skúlason, E.; Bligaard, T.; Norskov, J. K.
+# Scaling Properties of Adsorption Energies for Hydrogen-Containing Molecules on
+# Transition-Metal Surfaces. Phys. Rev. Lett. 2007, 99 (1), 016105
+# DOI: 10.1103/PhysRevLett.99.016105.
+abildpedersen_energies = { # Carbon, then Oxygen
+    'Pt':(-6.363636363636363,-3.481481481481482),
+    'Rh':(-6.5681818181818175,-4.609771721406942),
+    # 'Ir':(-6.613636363636363,-5.94916142557652),
+    # 'Au':(-3.7499999999999973,-2.302236198462614),
+    'Pd':(-6, -3.517877940833916),
+    # 'Cu':(-4.159090909090907,-3.85272536687631),
+    # 'Ag':(-2.9545454545454533,-2.9282552993244817),
+    'Ni':(-6.045454545454545,-4.711681807593758),
+    'Ru':(-6.397727272727272,-5.104763568600047),
+}
 
-tol_list = ['10_16_', '10_17_', '10_18_', '10_19_', '10_20_', '10_21_',
-            '10_22_', '10_23_', '10_24_', '11_16_', '11_17_', '11_18_',
-            '11_19_', '11_20_', '11_21_', '11_22_', '11_23_', '11_24_',
-            '12_16_', '12_17_', '12_18_', '12_19_', '12_20_', '12_21_',
-            '12_22_', '12_23_', '12_24_',]
+# "A Framework for Scalable Adsorbate-adsorbate Interaction Models"
+# Max J Hoffmann, Andrew J Medford, and Thomas Bligaard
+# From 2016 Hoffman et al.
+# https://doi.org/10.1021/acs.jpcc.6b03375
+hoffman_energies = { # Carbon, then Oxygen
+    'Pt':(-6.750,-3.586),
+    'Rh':(-6.78,-5.02),
+    'Ir':(-6.65,-4.73),
+    'Pd':(-6.58,-4.38),
+    # 'Cu':(-4.28,-4.51),
+    # 'Ag':(-2.91,-3.55),
+    'Ni':(-6.045454545454545,-4.711681807593758),# copied from abild pedersen
+    'Ru':(-6.397727272727272,-5.104763568600047),# copied from abild pedersen
+}
 
 
 def calculate(data):
-    ratio = data[1]
-    ch4_in = data[2]
-    ch4_out = data[3]
-    co_out = data[4]
-    h2_out = data[5]
-    h2o_out = data[6]
-    co2_out = data[7]
-    exit_T = data[8]
-    max_T = data[9]
-    dist_Tmax = data[10]
-    o2_conv = data[11]
+    index, ratio, ch4_in, ch4_out, co_out, h2_out, h2o_out, co2_out, exit_T, max_T, dist_Tmax, o2_conv, max_ch4_conv, dist_50_ch4_conv = data
 
     ch4_depletion = ch4_in - ch4_out
     ch4_conv = ch4_depletion / ch4_in
@@ -94,33 +110,19 @@ def calculate(data):
     return syngas_sel, syngas_yield, co_sel, co_yield, h2_sel, h2_yield, ch4_conv, fullox_sel, fullox_yield, exit_T, max_T, dist_Tmax, o2_conv
 
 
-def import_data(ratio, file_location):
+def import_data(ratio, f_location):
     """
-    This imports the data from the original simulation
+    This imports data from the original simulation
     """
-    try:
-        data = pd.read_csv('small-grid/' + file_location + '/all-data/' + tol + 'data.csv')
+    data = pd.read_csv(str(f_location) + '/avgdata.csv')
 
-        data = data.values
-        data = data.tolist()
-        return data
-    except:
-        print('Cannot find ' + file_location + '/all-data/' + tol + 'data.csv')
+    data = data.values
+    data = data.tolist()
 
-
-# For close packed surfaces from
-# Abild-Pedersen, F.; Greeley, J.; Studt, F.; Rossmeisl, J.; Munter, T. R.;
-# Moses, P. G.; Skúlason, E.; Bligaard, T.; Norskov, J. K.
-# Scaling Properties of Adsorption Energies for Hydrogen-Containing Molecules on
-# Transition-Metal Surfaces. Phys. Rev. Lett. 2007, 99 (1), 016105
-# DOI: 10.1103/PhysRevLett.99.016105.
-abildpedersen_energies = { # Carbon, then Oxygen
-'Ru': ( -6.397727272727272, -5.104763568600047),
-'Rh': ( -6.5681818181818175, -4.609771721406942),
-'Ni': ( -6.045454545454545, -4.711681807593758),
-'Pd': ( -6, -3.517877940833916),
-'Pt': ( -6.363636363636363, -3.481481481481482),
-}
+    for x in data:
+        if x[1] == ratio:
+            calculated_data = calculate(x)
+            return list(calculated_data)
 
 
 def lavaPlot(overall_rate, title, axis=False, folder=False, interpolation=True):
@@ -158,8 +160,9 @@ def lavaPlot(overall_rate, title, axis=False, folder=False, interpolation=True):
                        extent=extent2, aspect='equal', cmap="Spectral_r",
                        vmin=axis[0], vmax=axis[1],)
 
-    for metal, coords in abildpedersen_energies.items():
-        color = {'Ag':'k','Au':'k','Cu':'k'}.get(metal,'k')
+    for metal, coords in hoffman_energies.items():
+        # color = {'Ag':'k','Au':'k','Cu':'k'}.get(metal,'k')
+        color = {'Pt':'k','Rh':'k','Ir':'k','Pd':'k','Ni':'k','Ru':'k'}.get(metal,'k')
         plt.plot(coords[0], coords[1], 'o'+color)
         plt.text(coords[0], coords[1]-0.1, metal, color=color, fontsize=16)
     plt.xlim(carbon_range)
@@ -175,6 +178,7 @@ def lavaPlot(overall_rate, title, axis=False, folder=False, interpolation=True):
     if folder is False:
         plt.savefig(out_dir + '/' + str(title) +'.pdf', bbox_inches='tight')
     else:
+        os.path.exists(out_dir + '/' + str(folder)) or os.makedirs(out_dir + '/' + str(folder))
         plt.savefig(out_dir + '/' + str(folder) + '/' + str(title) +'.pdf', bbox_inches='tight')
     plt.show()
     plt.clf()
@@ -220,8 +224,9 @@ def lavaPlotAnimate(overall_rate, title, axis=False, folder=False, interpolation
                            extent=extent2, aspect='equal', cmap="Spectral_r",
                            vmin=axis[0], vmax=axis[1], animated=True)
         ims.append([im])
-    for metal, coords in abildpedersen_energies.items():
-        color = {'Ag':'k','Au':'k','Cu':'k'}.get(metal,'k')
+    for metal, coords in hoffman_energies.items():
+        # color = {'Ag':'k','Au':'k','Cu':'k'}.get(metal,'k')
+        color = {'Pt':'k','Rh':'k','Ir':'k','Pd':'k','Ni':'k','Ru':'k'}.get(metal,'k')
         plt.plot(coords[0], coords[1], 'o'+color)
         plt.text(coords[0], coords[1]-0.1, metal, color=color, fontsize=16)
     plt.xlim(carbon_range)
@@ -244,8 +249,13 @@ def lavaPlotAnimate(overall_rate, title, axis=False, folder=False, interpolation
         # ani.save(out_dir + '/' + str(folder) + '/' + str(title) + '.mpg', writer='ffmpeg', fps=5)
 
 
-array = os.listdir('./small-grid/')
+array = os.listdir('small-grid/')
 array = sorted(array)
+files = ['./small-grid/' + x for x in array]
+# array = os.listdir('small-grid-cov/')
+# array = sorted(array)
+# allfiles = files + ['./small-grid-cov/' + x for x in array] + ['./pt_cathub/','./pt_cathub_cov/']
+
 
 # for plotting
 c_s = []
@@ -265,61 +275,17 @@ sens_types = ['SynGasSelec', 'SynGasYield', 'COSelec', 'COYield', 'H2Selec',
               'MaxT', 'DistToMaxT', 'O2Conv']
 
 
-def average_data(data, type='avg'):
-    """
-    Averages the original simulation data from different tolerences
-    """
-    tol = len(data)
-    ratio = len(data[0])
-    value = len(data[0][0])
-
-    out = []
-    out_var = []
-    for r in range(ratio):
-        fixed_data = []
-        var_data = []
-        for s in range(2, value):
-            tmp_list = []
-            for t in range(tol):
-                tmp_list.append(data[t][r][s])
-
-            avg = statistics.mean(tmp_list)
-            var = statistics.variance(tmp_list)
-
-            fixed_data.append(avg)
-            var_data.append(var)
-        fixed_data.insert(0, data[0][r][1])
-        fixed_data.insert(0, data[0][r][0])
-        var_data.insert(0, data[0][r][1])
-        var_data.insert(0, data[0][r][0])
-        out.append(calculate(fixed_data))
-        out_var.append(var_data)
-
-    if type is 'avg':
-        return out
-    elif type is 'var':
-        return out_var
-
-
-def loadWorker(f_location):
+def loadWorker(ratio):
     data = []
 
-    for t in tol_list:
-        data.append(import_data(t, f_location))
+    array = os.listdir('small-grid/')
+    array = sorted(array)
+    files = ['./small-grid/' + x for x in array]
 
-    data_filter = [x for x in data if x] # filter out None values
-    data_avg = average_data(data_filter, type='avg')
-    data_var = average_data(data_filter, type='var')
+    for f in files:
+        data.append(import_data(ratio, f))
 
-    k = (pd.DataFrame.from_dict(data=data_avg, orient='columns'))
-    # k.columns = ['C/O ratio', 'CH4 in', 'CH4 out', 'CO out', 'H2 out', 'H2O out', 'CO2 out', 'Exit temp', 'Max temp', 'Dist to max temp', 'O2 conv', 'Max CH4 Conv', 'Dist to 50 CH4 Conv']
-    k.to_csv('linearscaling/' + f_location + '/avgdata.csv', header=True)
-
-    k = (pd.DataFrame.from_dict(data=data_var, orient='columns'))
-    # k.columns = ['C/O ratio', 'CH4 in', 'CH4 out', 'CO out', 'H2 out', 'H2O out', 'CO2 out', 'Exit temp', 'Max temp', 'Dist to max temp', 'O2 conv', 'Max CH4 Conv', 'Dist to 50 CH4 Conv']
-    k.to_csv('linearscaling/' + f_location + '/vardata.csv', header=True)
-
-    return data_avg
+    return data
 
 
 num_threads = len(ratios)
@@ -327,16 +293,6 @@ pool = multiprocessing.Pool(processes=num_threads)
 all_data = pool.map(loadWorker, ratios, 1)
 pool.close()
 pool.join()
-
-reordered_data = []
-for r in range(len(all_data[0])):
-    tmp = []
-    for b in range(len(all_data)):
-        tmp.append(all_data[b][r])
-    reordered_data.append(tmp)
-
-all_data = reordered_data  # rename to what it was before
-
 
 def spansWorker(sens):
     all_sens_data = []
@@ -380,6 +336,7 @@ pool.close()
 pool.join()
 
 ratios_index = list(range(len(ratios)))
+
 if max_cpus >= 28:
     num_threads = 28
     lump = 1
@@ -387,77 +344,36 @@ else:
     num_threads = max_cpus
     lump = int(28./max_cpus)
 pool = multiprocessing.Pool(processes=num_threads)
-pool.map_async(basePlotWorker, ratios_index, lump)
-pool.map_async(baseAnimateWorker, sens_index, lump)
+pool.map_async(basePlotWorker, ratios_index)
+pool.map_async(baseAnimateWorker, sens_index)
 pool.close()
 pool.join()
 
+# pool = multiprocessing.Pool(processes=15)
+# t = pool.map(basePlotWorker, ratios_index, 1)
+# pool.close()
+# pool.join()
+#
+# pool = multiprocessing.Pool(processes=15)
+# pool.map(baseAnimateWorker, sens_index, 1)
+# pool.close()
+# pool.join()
 
-def import_sensitivities(ratio, file_location):
+
+def import_sensitivities(ratio, file_location, avg='avg'):
     """
     Ratio is the C/O starting gas ratio
     file_location is the LSR C and O binding energy, false to load the base case
     """
-    tol, ratio = input
 
     try:
-        data = pd.read_csv('newdata/' + file_location + '/all-sensitivities/' + tol + str(ratio) + 'RxnSensitivity.csv')
+        data = pd.read_csv(file_location + '/avg-sensitivities/' + str(ratio) + avg + 'RxnSensitivity.csv')
 
         data = data.values
         data = data.tolist()
         return data
     except:
-        print('Cannot find ' + file_location + '/all-sensitivities/' + tol + str(ratio) + 'RxnSensitivity.csv')
-
-
-def average_sensitivities(data, type='avg'):
-    """
-    After loading in all the data at different ratios, average them all together
-    to calculate one "master" sensitivity value
-
-    Yes, it does both but only returns the one that is called.
-    Will rewrite to make it more efficient at a later point in time.
-    """
-    tol = len(data)
-    rxn = len(data[0])
-    sens = len(data[0][0])
-
-
-    out = []
-    out_var = []
-    for r in range(rxn):
-        fixed_data = []
-        var_data = []
-        for s in range(2, sens):
-            tmp_list = []
-            for t in range(tol):
-                tmp_list.append(data[t][r][s])
-
-            avg = statistics.mean(tmp_list)
-            var = statistics.variance(tmp_list)
-            data_new = np.array(tmp_list)
-            q25, q75 = np.percentile(data_new, 25), np.percentile(data_new, 75)
-            iqr = q75 - q25
-            cut_off = iqr * 2
-            lower, upper = q25 - cut_off, q75 + cut_off
-            outliers = [x for x in data_new if x < lower or x > upper]
-            outliers_removed = [x for x in data_new if x >= lower and x <= upper]
-            print(f"Removing {len(outliers)} outliers from {len(data_new)} sensitivity results for reaction {data[0][r][1]}")
-            avg = statistics.mean(outliers_removed)
-            var = statistics.variance(outliers_removed)
-            fixed_data.append(avg)
-            var_data.append(var)
-        fixed_data.insert(0, data[0][r][1])
-        fixed_data.insert(0, data[0][r][0])
-        var_data.insert(0, data[0][r][1])
-        var_data.insert(0, data[0][r][0])
-        out.append(fixed_data)
-        out_var.append(var_data)
-
-    if type is 'avg':
-        return out
-    elif type is 'var':
-        return out_var
+        print('Cannot find ' + file_location + '/avg-sensitivities/' + str(ratio) + avg + 'RxnSensitivity.csv')
 
 
 def loadSensDataWorker(f_location):
@@ -465,28 +381,8 @@ def loadSensDataWorker(f_location):
     sens_var = []
 
     for ratio in ratios:
-        allsens = []
-        for t in tol_list:
-            i = (t, ratio)
-            allsens.append(import_sensitivities(i, f_location))
-
-        allsens_filter = [x for x in allsens if x] # filter out None values
-        avg_data = average_sensitivities(allsens_filter, type='avg')
-        sensdata.append(avg_data)
-        var_data = average_sensitivities(allsens_filter, type='var')
-        sens_var.append(var_data)
-
-        k = (pd.DataFrame.from_dict(data=avg_data, orient='columns'))
-        # k.columns = ['Reaction', 'SYNGAS Selec', 'SYNGAS Yield', 'CO Selectivity', 'CO % Yield', 'H2 Selectivity', 'H2 % Yield',
-        #              'CH4 Conversion', 'H2O+CO2 Selectivity', 'H2O+CO2 yield', 'Exit Temp', 'Peak Temp',
-        #              'Dist to peak temp', 'O2 Conversion', 'Max CH4 Conv', 'Dist to 50 CH4 Conv']
-        k.to_csv(f"linearscaling/{f_location}/sensitivities/{ratio:.1f}avgRxnSensitivity.csv", header=True)
-
-        k = (pd.DataFrame.from_dict(data=var_data, orient='columns'))
-        # k.columns = ['Reaction', 'SYNGAS Selec', 'SYNGAS Yield', 'CO Selectivity', 'CO % Yield', 'H2 Selectivity', 'H2 % Yield',
-        #              'CH4 Conversion', 'H2O+CO2 Selectivity', 'H2O+CO2 yield', 'Exit Temp', 'Peak Temp',
-        #              'Dist to peak temp', 'O2 Conversion', 'Max CH4 Conv', 'Dist to 50 CH4 Conv']
-        k.to_csv(f"linearscaling/{f_location}/sensitivities/{ratio:.1f}varRxnSensitivity.csv", header=True)
+        sensdata.append(import_sensitivities(ratio, f_location))
+        sens_var.append(import_sensitivities(ratio, f_location, avg='var'))
 
     return sensdata
 
@@ -494,7 +390,7 @@ def loadSensDataWorker(f_location):
 num_threads = max_cpus
 lump = 1
 pool = multiprocessing.Pool(processes=num_threads)
-allrxndata = pool.map(loadSensDataWorker, array, lump)
+allrxndata = pool.map(loadSensDataWorker, files, lump)
 pool.close()
 pool.join()
 
@@ -504,6 +400,7 @@ for f in range(len(allrxndata)):  # for each lsr binding energy
         reactions.add(allrxndata[f][6][r][1])  # append the reaction itself
 reactions = list(reactions)
 
+sys.exit()
 
 def sensPlot(overall_rate, title, axis=False, folder=False):
     """
